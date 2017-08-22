8b76bd68dbaf243cb1ea5f54c270663a24525798
hqchen <hqchen@uchicago.edu>
2017-08-22 15:02:16 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Omega500AnalysisTools',
+      version='1.0',
+      description='omega500analysistools',
+      author='Camille, Huanqing',
+      author_email='hqchen@oddjob.uchicago.edu',
+      url='https://github.com/cavestruz/Omega500AnalysisTools/tree/master/omega500analysistools',
+      packages=['omega500analysistools'],
+     )
+
